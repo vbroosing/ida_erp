45fcfd1947dc4a28ec10e9c5a921a4438593dc0e
vbroosing <sanmartin.michels@gmail.com>
2026-03-09 01:18:07 -0300

    creando vistas subsistema operations, incluimos un templates en root y por el momento, un catalog/templates/catalog para mantener una estructura de carpetas interna de templates de manera organizada, además definimos algunas direcciones en el router para ingresar al template de lista de producto. finalmente establecí un diseño de vistas basadas en clases debido a la facilidad para implementar búsquedas basadas en id en el futuro

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Product
 
-# Create your views here.
+class ProductListView(ListView):
+    model = Product
+    template_name = 'catalog/product_list.html'
+    context_object_name = 'products'
+    
+    def get_queryset(self):
+        # Solo traemos los productos activos, optimizando la consulta
+        return Product.objects.filter(is_active=True).select_related('brand', 'category')
