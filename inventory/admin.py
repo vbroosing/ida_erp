140ed5e79731c1f8eb0f8f9c7e58ea99e5f2ae60
vbroosing <sanmartin.michels@gmail.com>
2026-03-09 00:52:27 -0300

    registrando en admin

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Warehouse, Location, Stock, StockAdjustment, Kardex
 
-# Register your models here.
+admin.site.register(Warehouse)
+admin.site.register(Location)
+admin.site.register(Stock)
+admin.site.register(StockAdjustment)
+admin.site.register(Kardex)
