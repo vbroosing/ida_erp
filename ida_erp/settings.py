39ac4932645e4e57bd370ea28908ba04727ad672
vbroosing <sanmartin.michels@gmail.com>
2026-03-09 23:25:24 -0300

    configurando staticfiles, annadiendo par de .js para mejorar UX/UI, desacoplando html

--- ida_erp/settings.py
+++ ida_erp/settings.py
@@ -117,3 +117,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+# Le decimos a Django dónde buscar archivos estáticos globales
+STATICFILES_DIRS = [BASE_DIR / "static"]
